a58f05d2809e0ba6f5413e519a589fde82f2e9a6
Kevin Gutiérrez <>
2018-03-26 00:51:10 -0500

    Add deployment script

---
+++ scripts/deploy-ghpages.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# ideas used from https://gist.github.com/motemen/8595451
+
+# abort the script if there is a non-zero error
+set -e
+
+# show where we are on the machine
+pwd
+
+# now lets setup a new repo so we can update the gh-pages branch
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
+
+# Push, but send any output to /dev/null to hide anything sensitive
+git subtree push --prefix dist origin gh-pages > /dev/null 2>&1
+
+echo "Finished Deployment!"
